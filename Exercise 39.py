3aed20aac1e1393b91858fc85d1e0f6792d5676f
vothidieuthuy <146844989+vothidieuthuy@users.noreply.github.com>
2023-11-11 08:48:26 +0700

    Exercise 39.py

--- Exercise 39.py
+++ Exercise 39.py
@@ -2,20 +2,20 @@ de=float(input('Enter the number of decibels:'))
 if (de>0) and (de<40):
     print('Sound level is quieter than a quiet room')		
 elif (de==40):
-    print('Sound level is equivalent to a quiet room.')	
+    print('Sound level is equivalent to a quiet room')	
 elif (de>40) and (de<70):
-    print('Sound level is quieter than an alarm clock.')
+    print('Sound level is between quiet room and alarm clock')
 elif (de==70):
     print('Sound level is equivalent to an alarm clock')		
 elif (de>70) and (de<106):
-    print('Sound level is quieter than a lawn mower.')	
+    print('Sound level is between alarm clock and lawnmower')	
 elif (de==106):
-    ('Sound level is equivalent to a lawn mower.')	
+    print('Sound level is equivalent to a lawnmower')	
 elif (de>106) and (de<130):
-    print("Sound level is quieter than a jackhammer.")		
+    print('Sound level is between lawnmower and jackhammer')		
 elif (de==130):
-    print('Sound level is equivalent to a jackhammer.')		
+    print('Sound level is equivalent to a jackhammer')		
 elif (de>130):
-    print('Sound level is way too loud.')		
+    print('Sound level is way too loud')		
 else:
     print('Error number')		
